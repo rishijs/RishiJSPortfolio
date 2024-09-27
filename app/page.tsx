b02890e994b4089b650892e68beed633fd4d2bb3
rishijs <rishijs2002@gmail.com>
2024-09-26 21:09:17 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,27 @@ import { UpdatePosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio - Rishi JS
+      <h2 className="mb-2 text-2xl font-semibold tracking-tighter">
+        Name, picture, email
+      </h2>
+      <h1 className="mb-4">
+        {`Tagline and external links`}
       </h1>
-      <p className="mb-4">
-        {`This portfolio is a work in progress`}
-      </p>
       <div className="my-8">
         <UpdatePosts />
       </div>
+      <p className="mb-4">
+        {` Summary, Resume download`}
+      </p>
+      <p className="mb-4">
+        {`Education`}
+      </p>
+      <p className="mb-4">
+        {`Skills`}
+      </p>
+      <p className="mb-4">
+        {`Contact Button`}
+      </p>
     </section>
   )
 }
