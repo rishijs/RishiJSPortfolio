c888d6698e7f279cabd16e0867837adfa27055bb
rishijs <rishijs2002@gmail.com>
2024-10-01 02:39:06 -0400

    content update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,15 @@ export default function Page() {
   return (
     <section>
       <h2 className="mb-2 text-2xl font-semibold tracking-tighter">
-        Name, picture, email
+        {`Rishi Jammalamadaka, rishi.rjsd@gmail.com`}
+        <img src="/rishi1.jpg" width='100px' height='100px'/>
       </h2>
       <h1 className="mb-4">
-        {`Tagline and external links`}
+        {`Software Engineer and Game Developer
+        B.S. Computer Science @Rutgers New Brunswick
+        
+        Working on Freelance and Personal Projects
+        Looking for Full Time Roles`}
       </h1>
       <div className="my-8">
         <UpdatePosts />
@@ -16,13 +21,24 @@ export default function Page() {
         {` Summary, Resume download`}
       </p>
       <p className="mb-4">
-        {`Education`}
+        {`Education: Rutgers University, New Brunswick
+         Bachelor of Science (B.S.) Major in Computer Science
+         GPA 3.44 January 2024
+
+        Data Management & Data Science, Artificial Intelligence, 
+        Computer Algorithms, Data Structures, Computer Architecture, 
+        Systems Programming, Imaging & Multimedia, Calculus,
+        Linear Algebra, Discrete Math, Numerical Analysis`}
       </p>
       <p className="mb-4">
-        {`Skills`}
+        {`Python, Javascript, Java, C/C++/C#, GDScript, HTML/CSS, JDBC,
+          MySQL Tools/Frameworks: Three.js, React.js, Flask, Git, 
+          Unreal Engine, Unity, Godot, Jupyter Notebooks,
+          Pandas, NumPy, Matplotlib, Pygame`}
       </p>
       <p className="mb-4">
-        {`Contact Button`}
+        {`New Jersey - rishi.rjsd@gmail.com
+        Contact Button/Info`}
       </p>
     </section>
   )
