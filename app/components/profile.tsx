03d924ddc9f42e8319e28a6c2fea4fc17d09cea3
rishijs <rishijs2002@gmail.com>
2025-03-06 19:59:38 -0500

    profile formatting

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -9,21 +9,30 @@ export default async function Profile() {
 
   return (
     <div className="grid gap-2 grid-cols-1">
-        <div className="grid grid-cols-3 gap-8 text-2xl font-semibold tracking-tighter">
-            <div className="grid grid-cols-1">
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-xl md:text-2xl font-semibold tracking-tighter items-center">
+            <div className="flex flex-col items-center md:items-start">
                 <h2 className="mb-2">
                     {`Rishi Jammalamadaka, rishi.rjsd@gmail.com`}
                 </h2>
-                <Link href="/resume.pdf" target="_blank"
-                    className="text-center border border-solid border-white bg-foreground h-fit w-48 hover:!bg-red-500">
+                <Link
+                    href="/resume.pdf"
+                    target="_blank"
+                    className="text-center border border-solid border-white bg-foreground h-fit w-48 hover:!bg-red-500 p-2"
+                >
                     View Resume
                 </Link>
             </div>
-            <img src="/rishi4.jpg" className="ml-2 w-fit h-fit"/>
-            <div className="h-fit">
+
+            <div className="flex justify-center">
+                <img src="/rishi4.jpg" className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full" />
+            </div>
+
+            <div className="h-fit flex justify-center md:justify-start">
                 <ProfileNotables />
             </div>
         </div>
+
         
         <h1 className="mb-4">
             {/*`Full Stack Developer & Jack of All Trades`*/}
